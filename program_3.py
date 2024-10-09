73e71bca12e7fd93b77ee4518e7f21c45379d54f
JadonAnderstrom <jjanderstrom@students.unwsp.edu>
2024-10-08 19:32:33 -0500

    Update program_3.py

--- program_3.py
+++ program_3.py
@@ -20,13 +20,16 @@ def calculate_sales_tax(total_sales):
 
   return county_sales_tax, state_sales_tax, total_sales_tax
 
-# Get total sales from the user
+# Get total sales
 total_sales = float(input("Enter the total sales for the month: $"))
 
 # Calculate sales taxes
 county_sales_tax, state_sales_tax, total_sales_tax = calculate_sales_tax(total_sales)
 
+
 # Print the results
 print("County Sales Tax: $", county_sales_tax)
 print("State Sales Tax: $", state_sales_tax)
 print("Total Sales Tax: $", total_sales_tax)
+
+#Jadon Anderstrom, 10/08/24, "Tax Rate".
